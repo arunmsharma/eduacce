fddc784bd8099013e295848a1be2ac6cd994a8a3
Arun Mohan Sharma <arun-mark-iii@Aruns-MacBook-Pro.local>
2023-02-10 10:50:27 +0530

    spring boot auto configuration

--- src/main/java/com/arun/eduacce/EduacceSchoolApplication.java
+++ src/main/java/com/arun/eduacce/EduacceSchoolApplication.java
@@ -2,7 +2,9 @@ package com.arun.eduacce;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
+//@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 @SpringBootApplication
 public class EduacceSchoolApplication {
 
