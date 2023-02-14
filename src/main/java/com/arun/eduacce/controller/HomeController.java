0f891a22d148c0f3ac6a695eeed6b40eab8573ae
Arun Mohan Sharma <arun-mark-iii@Aruns-MBP.bbrouter>
2023-02-14 23:11:53 +0530

    dynamic template with thymeleaf

--- src/main/java/com/arun/eduacce/controller/HomeController.java
+++ src/main/java/com/arun/eduacce/controller/HomeController.java
@@ -1,13 +1,15 @@
 package com.arun.eduacce.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
 
-    @RequestMapping("/home")
-    public String displayHomePage(){
+    @RequestMapping(value = {"","/","/home"})
+    public String displayHomePage(Model model){
+        model.addAttribute("username","Arun Mohan");
         return "home.html";
     }
 }
