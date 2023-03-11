c7792599cc839eeaec4a358400add012015c46f7
Arun Mohan Sharma <arun-mark-iii@Aruns-MacBook-Pro.local>
2023-03-11 12:46:33 +0530

    adding rest services for deleting and closing a contact ms

--- src/main/java/com/arun/eduacce/rest/ContactRestController.java
+++ src/main/java/com/arun/eduacce/rest/ContactRestController.java
@@ -1,16 +1,21 @@
 package com.arun.eduacce.rest;
 
+import com.arun.eduacce.Constants.EduacceConstants;
 import com.arun.eduacce.model.Contact;
 import com.arun.eduacce.model.Response;
 import com.arun.eduacce.repository.ContactRepository;
 import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Slf4j
 @RestController
@@ -46,5 +51,38 @@ public class ContactRestController {
         response.setStatusCode("200");
         response.setStatusMsg("Message saved successfully");
         return ResponseEntity.status(HttpStatus.CREATED).header("isMsgSaved","true").body(response);
+    }
+
+    @DeleteMapping("/deleteMsg")
+    public ResponseEntity<Response> deleteMsg(RequestEntity<Contact> requestEntity){
+        HttpHeaders headers = requestEntity.getHeaders();
+        headers.forEach((key,value)->{
+            log.info(String.format("Header '%s' = '%s'",key,value.stream().collect(Collectors.joining("|"))));
+        });
+        Contact contact = requestEntity.getBody();
+        contactRepository.deleteById(contact.getContactId());
+        Response response = new Response();
+        response.setStatusCode("200");
+        response.setStatusMsg("Message deleted successfully");
+        return ResponseEntity.status(HttpStatus.OK).body(response);
+    }
+
+    //patch mapping should be used when we are updating a data partially inside database as per http standards
+    @PatchMapping("/closeMsg")
+    public ResponseEntity<Response> closeMsg(@RequestBody Contact reqContact){
+        Response response = new Response();
+        Optional<Contact> contact = contactRepository.findById(reqContact.getContactId());
+        if(contact.isPresent()){
+            contact.get().setStatus(EduacceConstants.CLOSE);
+            contactRepository.save(contact.get());
+        }else{
+            response.setStatusCode("400");
+            response.setStatusMsg("Invalid contact id received");
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                    .body(response);
+        }
+        response.setStatusCode("200");
+        response.setStatusMsg("contact msg closed successfully");
+        return ResponseEntity.status(HttpStatus.OK).body(response);
     }
 }
